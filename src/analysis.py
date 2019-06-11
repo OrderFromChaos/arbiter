3e924d5700974bdabd8c2545fb820f4ed60eba61
Syris Norelli <snore001@ucr.edu>
2019-06-10 20:21:21 -0700

    Standardized analysis helper functions, started on optimal portfolio allocation problem

--- src/analysis.py
+++ src/analysis.py
@@ -48,6 +48,7 @@ def basicTest(strategy, inputs=None, printsat=True):
 
 ### HELPER FUNCTIONS ###
 
+# Numerical functions
 def simpleMovingAvg(L, n):
     if len(L) < n:
         return 'N is too high for this list (' + str(len(L)) + ' < ' + str(n) + ')'
@@ -57,25 +58,6 @@ def simpleMovingAvg(L, n):
         avg = sum(L[i:i+n])/n
         avg_list.append(avg)
     return avg_list
-
-def removeOutliers(dataset, key, sigma):
-    # Filter all data points that are greater than sigma away from the mean
-    outlier_axis = [x[key] for x in dataset]
-
-    mean = sum(outlier_axis)/len(outlier_axis)
-    stdev = (sum([(x-mean)**2 for x in outlier_axis])/len(outlier_axis))**0.5
-
-    return [x for x in dataset if mean-sigma*stdev < x[key] < mean+sigma*stdev]
-
-def volumeFilter(dataset, saleslastmonth): # Run for all strategies by default
-    return [x for x in dataset if len(x['Sales from last month']) >= saleslastmonth]
-
-def listingFilter(dataset, amountoflistings):
-    return [x for x in dataset if len(x['Listings']) >= amountoflistings]
-
-def profiler(items):
-    # to be built later; used to mine frequency data from the items that satisfy a filter
-    pass
 
 def median(L):
     length = len(L)
@@ -87,9 +69,13 @@ def median(L):
         return L[pos], pos
     else:
         rightmid = length//2
-        return (L[rightmid-1]+L[rightmid])/2, rightmid-0.5
+        try:
+            return (L[rightmid-1]+L[rightmid])/2, rightmid-0.5
+        except:
+            raise Exception('!!! ' + str(L))
 
 def quartiles(L):
+    assert len(L) >= 3, 'Quartiles are meaningless with less than 3 data points'
     L = sorted(L)
 
     Q2, midpoint = median(L)
@@ -101,19 +87,49 @@ def quartiles(L):
         Q3, Q3pos = median(L[ceil(midpoint):])
     return (Q1,Q2,Q3), (Q1pos, midpoint, Q3pos)
 
-def dateFilter(dataset, ndays):
-    for index, item in enumerate(dataset):
-        filter_date = item['Sales from last month'][-1][0] - timedelta(days=ndays)
-        dataset[index]['Sales from last month'] = [x for x in item['Sales from last month'] 
-                                                   if x[0] >= filter_date]
-    return dataset
 
-def tail(dataset,n):
+# Functions that check whether items satisfy a condition and return a bool
+def volumeFilter(item, saleslastmonth):
+    if len(item['Sales from last month']) >= saleslastmonth:
+        return True
+    return False
+
+def listingFilter(item, amountoflistings):
+    if len(item['Listings']) >= amountoflistings:
+        return True
+    return False
+
+
+# Functions that act on an item subgroup and return a filtered list
+def removeOutliers(item, key, sigma):
+    # Filter all data points that are greater than sigma away from the mean
+    outlier_axis = item[key] # eg, listings
+
+    mean = sum(outlier_axis)/len(outlier_axis)
+    stdev = (sum([(x-mean)**2 for x in outlier_axis])/len(outlier_axis))**0.5
+
+    item[key] = [x for x in outlier_axis if mean-sigma*stdev < x < mean+sigma*stdev]
+    return item
+
+def historicalDateFilter(item, ndays):
+    filter_date = item['Date'] - timedelta(days=ndays)
+    historical = item['Sales from last month']
+    if isinstance(historical[0],list): # Fed in as standard [[datetime, price],...] format
+        item['Sales from last month'] = [x for x in historical if x[0] >= filter_date]
+    else:
+        raise Exception('Unrecognized instance! ' + str(historical[0]))
+    return item
+
+def profiler(dataset):
+    # to be built later; used to mine frequency data from the items that satisfy a filter
+    pass
+
+# Functions that work on a dataset
+def head(dataset,n): # Similar to *nix "head -n 3 /etc/datafile"
     for i in dataset[:n]:
         print(i)
 
 ########################
-
 
 
 ### ACTUAL STRATEGY ###
@@ -124,7 +140,7 @@ class SimpleListingProfit:
                                      # For CS:GO, this is 15%
     def run(self, dataset):
         outputs = []
-        dataset = listingFilter(dataset,4)
+        dataset = [x for x in dataset if listingFilter(x,4)]
         for item in dataset:
             itemdict = dict()
             listings = item['Listings']
@@ -143,30 +159,26 @@ class SimpleListingProfit:
 class LessThanThirdQuartileHistorical:
     def run(dataset):
         outputs = []
-        # dataset = dateFilter(dataset,15)
-        dataset = volumeFilter(dataset,30) # Q3 is meaningless without this
-        # dataset = removeOutliers(dataset,'Sales from last month',2) # Bugged for some reason
         for item in dataset:
             itemdict = LessThanThirdQuartileHistorical.runindividual(item)
-            # itemdict['Expected Profit/Day'] = round(Q3-itemdict['Lowest Listing']*1.15,2)
             outputs.append(itemdict)
         return outputs
     def runindividual(item):
+        # Assume already volume filtered (>30 sales last month recommended)
         itemdict = dict()
-        # Assume already volume filtered
+        item = historicalDateFilter(item,15)
         historical = [x[1] for x in item['Sales from last month']]
 
-        quarts, anchors = quartiles(historical)
+        quarts, _ = quartiles(historical)
         Q1, Q2, Q3 = quarts
-        _, _, Q3pos = anchors
-        # Q3pos is for the later to be implemented Expected Profit/Day
 
         itemdict['Satisfied'] = (item['Listings'][0]*1.15 < Q3 
                                     and item['Special Type'] != 'Souvenir')
         itemdict['Name'] = item['Item Name']
         itemdict['Quartiles'] = tuple(map(lambda x: round(x,2), (Q1,Q2,Q3)))
         itemdict['Lowest Listing'] = item['Listings'][0]
-        itemdict['Sales/Day'] = item['Sales/Day']
+        itemdict['Days To Profit'] = round(1/(item['Sales/Day']/4),2)
+        # ^^ x/4 because 1/4th chance to appear at or above Q3
         itemdict['Expected Profit'] = round(Q3-itemdict['Lowest Listing']*1.15,2)
 
         return itemdict
@@ -175,33 +187,36 @@ class SpringSearch:
     # Items whose historical data Q1 and Q3 differ by more than 15%
     def run(dataset):
         outputs = []
-        dataset = volumeFilter(dataset,30) # Spring is likely not meaningful without large vol.
-        dataset = dateFilter(dataset,15)
         for item in dataset:
             itemdict = SpringSearch.runIndividual(item)
             outputs.append(itemdict)
         return outputs
     def runIndividual(item):
-        # Assume already volume filtered
+        # Assume historical has >= 3 sales
         itemdict = dict()
         cutoff_ratio = 1.15
-        item = dateFilter([item],15)[0]
+        # item = historicalDateFilter(item,7)
 
         historical = [x[1] for x in item['Sales from last month']]
-        quarts, anchors = quartiles(historical)
+        quarts, _ = quartiles(historical)
         Q1, Q2, Q3 = quarts
 
         itemdict['Satisfied'] = (item['Special Type'] != 'Souvenir'
+                                # Souvenirs aren't very predictable imo
                                     and Q3/cutoff_ratio > item['Buy Rate']
-                                    and Q1*cutoff_ratio < Q3
-                                    and item['Sales/Day'] >= 2)
+                                    # If buy rate is higher than profit pt., no chance of profit
+                                    and Q1*cutoff_ratio < Q3 
+                                    # Actual profit filter
+                                    and quarts[0] <= 100
+                                    # Account balance is limited
+                                )
         itemdict['Name'] = item['Item Name']
         itemdict['Quartiles'] = tuple(map(lambda x: round(x,2), quarts))
-        itemdict['Sales/Day'] = item['Sales/Day']
+        itemdict['Days/Profit'] = round(1/(item['Sales/Day']/8),2)
+        # ^^ x/8 because 1/4th chance to appear at or below Q1, 1/4th at or above Q3
         itemdict['Ratio'] = round(Q3/Q1,2)
         itemdict['Profit'] = round((itemdict['Ratio']-1.15)*Q1,2) # Buy at Q1, sell at Q3
         itemdict['Profit/Day'] = round(itemdict['Profit']*item['Sales/Day']/8,2)
-                            # /8 because 1/4th chance to appear at or below Q1, 1/4th at or above Q3
 
         return itemdict
 
@@ -211,14 +226,14 @@ if __name__ == '__main__':
     print('Doing inital dataset import...')
     DBdata = import_json_lines('../data/pagedata.txt', encoding='utf_16', numlines=11)
     print('Successful import! Number of entries:', len(DBdata))
-    DBdata = volumeFilter(DBdata, 30)
-    DBdata = listingFilter(DBdata, 1)
+    DBdata = [x for x in DBdata if volumeFilter(x, 30)]
+    DBdata = [x for x in DBdata if listingFilter(x, 1)]
     print('Number that satisfy volume and listing filter:', len(DBdata))
     print()
 
     # Testing SimpleListingProfit
     SLPresults, SLPsatresults = basicTest(SimpleListingProfit,inputs=[1.15], printsat=False)
-    tail(sorted(SLPsatresults, key = lambda x: x['Ratio'], reverse=True),10)
+    head(sorted(SLPsatresults, key = lambda x: x['Ratio'], reverse=True),10)
     print()
 
     # Testing LessThanThirdQuartileHistorical
@@ -228,5 +243,8 @@ if __name__ == '__main__':
     # Testing SpringSearch
     SSresults, SSsatresults = basicTest(SpringSearch, printsat=False)
     SSsatresults = sorted(SSsatresults, key = lambda x: x['Profit/Day'], reverse=True)
-    tail(SSsatresults,10)
-    print('Average daily profit at perfect information:', round(sum([x['Profit/Day'] for x in SSsatresults]),2))
+    head(SSsatresults,10)
+    print('Average daily profit at perfect information/liquidity:', round(sum([x['Profit/Day'] for x in SSsatresults]),2))
+    portfolio_size = 100
+    print('Highest profit at',portfolio_size)
+    
---
+++ src/optimal_allocation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Author: Syris Norelli, snore001@ucr.edu
+
+### PURPOSE:
+### Given cost to limited unit A and profit in unit B, how to maximize unit B profit?
+### This is the 0/1 knapsack problem...
+### https://en.wikipedia.org/wiki/Knapsack_problem#0/1_knapsack_problem
+### However, it uses strictly positive real numbers instead of strictly positive integers.
+
+def knapsack(costs, values, costlimit):
+    assert len(costs) == len(values), ('Mismatched number of costs and values ' + 
+                                       (len(costs),len(values)))
+    n = range(len(costs))
+    maxdp = {0:0} # Base case
+
+    for itemcount in range(1,n):
+        cost = costs[itemcount]
+        previous = maxdp[itemcount-1]
+        if cost > costlimit:
+            maxdp[itemcount] = previous
+        else:
+            maxdp[itemcount] = max(previous, previous + )
+            newsum = maxdp[itemcount-1] + cost
+
